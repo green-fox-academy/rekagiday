1d795ec98e38e332b74ec6ee7ecc8d24f62c8339
Reka Giday <giday.r@gmail.com>
2017-03-22 14:05:43 +0100

    initial commit

---
+++ week-02/day-3/src/SumElements.java
@@ -0,0 +1,9 @@
+/**
+ * Created by User on 2017. 03. 22..
+ */
+public class SumElements {
+  public static void main(String[] args) {
+    int[] r = {54, 23, 66, 12};
+    System.out.println("The sum of the 2nd and 3rd elements is: " + (r[1] + r[2]));
+  }
+}
