f8a6b474584c81af6549a5165c36d77f9f698521
Reka Giday <giday.r@gmail.com>
2017-03-22 14:08:15 +0100

    initial commit

---
+++ week-02/day-3/src/ChangeElement.java
@@ -0,0 +1,13 @@
+/**
+ * Created by User on 2017. 03. 22..
+ */
+public class ChangeElement {
+  public static void main(String[] args) {
+    int[] s = {1, 2, 3, 8, 5, 6};
+
+    s[3] = 4;
+
+    System.out.println("The 4th element is: " + s[3]);
+
+  }
+}
