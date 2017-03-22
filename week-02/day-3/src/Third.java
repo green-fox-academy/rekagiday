fefa4db6a35f85568e7c56744d9971ad73241c4e
Reka Giday <giday.r@gmail.com>
2017-03-22 13:57:35 +0100

    initial commit

---
+++ week-02/day-3/src/Third.java
@@ -0,0 +1,10 @@
+/**
+ * Created by User on 2017. 03. 22..
+ */
+public class Third {
+  public static void main(String[] args) {
+    int[] q = {4, 5, 6, 7};
+    System.out.println("The third element is: " + q[2]);
+    }
+  }
+
