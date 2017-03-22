60da4ab2bad4665f9ceecdf01b76abd53063dc49
Reka Giday <giday.r@gmail.com>
2017-03-22 14:13:34 +0100

    initial commit

---
+++ week-02/day-3/src/PrintAll.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+/**
+ * Created by User on 2017. 03. 22..
+ */
+public class PrintAll {
+  public static void main(String[] args) {
+
+    int[] af = {4, 5, 6, 7};
+    System.out.println(Arrays.toString(af));
+  }
+}
