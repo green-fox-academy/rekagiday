1530ab4fcd6e4dea4c5345033a80edc742ddc353
Reka Giday <giday.r@gmail.com>
2017-03-22 16:23:45 +0100

    fixed my ugly indentation

--- week-02/day-3/src/DrawPyramid.java
+++ week-02/day-3/src/DrawPyramid.java
@@ -1,27 +1,26 @@
 import java.util.Scanner;
-
 /**
  * Created by User on 2017. 03. 22.
  */
 public class DrawPyramid {
 
-    public static void main(String[] args) {
-      Scanner scanner = new Scanner(System.in);
-      System.out.println("Please type a number to indicate how big your pyramid should be");
-      int lines = scanner.nextInt();
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Please type a number to indicate how big your pyramid should be");
+    int lines = scanner.nextInt();
 
-      for (int i = 1; i <= lines; i++) {
-        String s = new String();
-        for (int j = lines; j >= 1; j--) {
-          if (j > i) {
-            s += " ";
-          } else if (j == i) {
-            s += "*";
-          } else {
-            s += "**";
-          }
+    for (int i = 1; i <= lines; i++) {
+      String a = new String();
+      for (int j = lines; j >= 1; j--) {
+        if (j > i) {
+          a += " ";
+        } else if (j == i) {
+          a += "*";
+        } else {
+          a += "**";
         }
-        System.out.println(s);
       }
+      System.out.println(a);
     }
+  }
 }
