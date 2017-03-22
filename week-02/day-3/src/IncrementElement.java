5f846d100c8c2d50e194eb5c3c8cf0121785ea40
Reka Giday <giday.r@gmail.com>
2017-03-22 14:10:50 +0100

    initial commit

---
+++ week-02/day-3/src/IncrementElement.java
@@ -0,0 +1,10 @@
+/**
+ * Created by User on 2017. 03. 22..
+ */
+public class IncrementElement {
+  public static void main(String[] args) {
+    int[] t ={1, 2, 3, 4, 5};
+    t[2]++;
+    System.out.println("The third element is: " + t[2]);
+  }
+}
