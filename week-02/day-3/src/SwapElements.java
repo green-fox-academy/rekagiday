661117f1bf4f039914f7eadd98efce5d5f90a36f
Reka Giday <giday.r@gmail.com>
2017-03-22 15:15:45 +0100

    initial commit

---
+++ week-02/day-3/src/SwapElements.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+/**
+ * Created by User on 2017. 03. 22..
+ */
+public class SwapElements {
+  public static void main(String[] args) {
+    String[] abc = {"first", "second", "third"};
+    String placeholder = abc[0];
+    abc[0] = abc[2];
+    abc[2] = placeholder;
+
+    System.out.println(Arrays.toString(abc));
+
+  }
+}
