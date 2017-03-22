d5bb4e2bc7f00a00a98a98b37c95bb53e8ebdfdd
Reka Giday <giday.r@gmail.com>
2017-03-22 15:21:16 +0100

    initial commit

---
+++ week-02/day-3/src/SumAll.java
@@ -0,0 +1,16 @@
+/**
+ * Created by User on 2017. 03. 22..
+ */
+public class SumAll {
+  public static void main(String[] args) {
+
+    int[] ai = {3, 4, 5, 6, 7};
+    int sumAll = 0;
+
+    for (int i = 0; i < ai.length; i++){
+
+      sumAll += ai[i];
+      }
+    System.out.println(sumAll);
+  }
+}
