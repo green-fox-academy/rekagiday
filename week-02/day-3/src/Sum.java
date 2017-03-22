7c4ebda84e80d4bb9cf86987091854bd2afa66f7
Reka Giday <giday.r@gmail.com>
2017-03-22 12:05:03 +0100

    fixed for loop

--- week-02/day-3/src/Sum.java
+++ week-02/day-3/src/Sum.java
@@ -2,12 +2,16 @@
  * Created by User on 2017. 03. 22..
  */
 public class Sum {
-  public static void sum(int[] array) {
-    System.out.println();
-
+  public static void sum(int a) {
+    int total = 0;
+    for (int i = 1; i < a + 1; i++) {
+      total = total + i;
+    }
+    System.out.println(total);
   }
+
   public static void main(String[] args) {
-    sum(1,2);
+    sum(5);
 
   }
 }
