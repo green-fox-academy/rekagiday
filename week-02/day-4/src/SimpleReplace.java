ade73f2698bde2b43d5cd41ca9bd628404686eca
Reka Giday <giday.r@gmail.com>
2017-03-23 10:52:18 +0100

    inital commit of exercise01

---
+++ week-02/day-4/src/SimpleReplace.java
@@ -0,0 +1,13 @@
+/**
+ * Created by User on 2017. 03. 23..
+ */
+public class SimpleReplace {
+
+  public static void main(String[] args) {
+    String example = "In a dishwasher far far away";
+
+    example = example.replace ("dishwasher", "galaxy");
+
+    System.out.println(example);
+  }
+}
