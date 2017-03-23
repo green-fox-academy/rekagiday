83fac69b1ca32c1b555e911851a71d94667f8a49
Reka Giday <giday.r@gmail.com>
2017-03-23 15:16:43 +0100

    added funcional reverse code

--- week-02/day-4/src/Reverse.java
+++ week-02/day-4/src/Reverse.java
@@ -1,26 +1,15 @@
-import com.sun.org.apache.regexp.internal.RE;
 
 /**
  * Created by User on 2017. 03. 23..
  */
-//public class Reverse {
-//
-//  public static void ReverseText(String reverse) {
-//
-//    int length = reverse.length();
-//
-//    for (int i = 0; i < length / 2; i++) {
-//      int l = reverse.length()-1;
-//      char last = reverse.charAt(l);
-//      char placeholder = reverse.charAt(i);
-//      reverse.charAt(i) = reverse.charAt(l);
-//      reverse.charAt(l) = placeholder;
-//      }
-//    return reverse;
-//  }
-//
-//  public static void main(String[] args) {
-//    ReverseText(".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI");
-//    System.out.println(ReverseText();
-//  }
-//}
+public class Reverse {
+
+  public static void main(String[] args) {
+
+    String text =".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
+
+    text = new StringBuilder(text).reverse().toString();
+
+    System.out.println(text);
+  }
+}
