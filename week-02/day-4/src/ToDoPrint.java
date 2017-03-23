e113700603e1ac21b7d325a4e474c6901c605ebe
Reka Giday <giday.r@gmail.com>
2017-03-23 11:47:45 +0100

    inital commit of exercise04

---
+++ week-02/day-4/src/ToDoPrint.java
@@ -0,0 +1,11 @@
+/**
+ * Created by User on 2017. 03. 23..
+ */
+public class ToDoPrint {
+
+  public static void main(String[] args) {
+    String todoText = "My todo list for today: \n - Buy milk\n - Download games\n   - Diablo";
+    System.out.println(todoText);
+  }
+
+}
