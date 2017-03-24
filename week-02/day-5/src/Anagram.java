09d8b82f56a6cf7f56cecf0ee6d25a43e8f0c1b3
Reka Giday <giday.r@gmail.com>
2017-03-24 11:24:32 +0100

    changed anagram method to bubble sort + compare

--- week-02/day-5/src/Anagram.java
+++ week-02/day-5/src/Anagram.java
@@ -9,19 +9,40 @@ public class Anagram {
 
     Scanner scanner = new Scanner(System.in);
     System.out.println("Please give me two words and I can tell if they are anagram");
+
     String firstWord = scanner.nextLine();
+    firstWord = firstWord.toLowerCase();
+    for (int i = 0; i < firstWord.length(); i++) {
+      for (int j = i; j < firstWord.length(); j++) {
+        if (firstWord.charAt(i) > firstWord.charAt(j)) {
+          firstWord = firstWord.substring(0, i)
+              + firstWord.charAt(j)
+              + firstWord.substring(i + 1, j)
+              + firstWord.charAt(i)
+              + firstWord.substring(j + 1, firstWord.length());
+        }
+      }
+    }
     String secondWord = scanner.nextLine();
+    for (int i = 0; i < secondWord.length(); i++) {
+      for (int j = i; j < secondWord.length(); j++) {
+        if (secondWord.charAt(i) > secondWord.charAt(j)) {
+          secondWord = secondWord.substring(0, i)
+              + secondWord.charAt(j)
+              + secondWord.substring(i + 1, j)
+              + secondWord.charAt(i)
+              + secondWord.substring(j + 1, secondWord.length());
+        }
+      }
+    }
 
     if (firstWord.length() != secondWord.length()) {
       System.out.println(false);
-    } else {
-      for (int i = 0; i < firstWord.length(); i++) {
-        if (secondWord.indexOf(firstWord.charAt(i)) == -1) {
-          System.out.println(false);
-          return;
-        }
-      }
+    } else if (firstWord.equals(secondWord)) {
       System.out.println(true);
+      return;
+    } else {
+      System.out.println(false);
     }
   }
 }
