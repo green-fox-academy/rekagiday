3141fe5939d0f1807d8dc90d29cadb5ae9b3e304
Reka Giday <giday.r@gmail.com>
2017-03-24 10:55:03 +0100

    user input for words added

--- week-02/day-5/src/Anagram.java
+++ week-02/day-5/src/Anagram.java
@@ -1,17 +1,21 @@
+import java.util.Scanner;
+
 /**
  * Created by User on 2017. 03. 23..
  */
 public class Anagram {
 
   public static void main(String[] args) {
-    String firstWord = "car";
-    String secondWord = "arc";
+
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Please give me two words and I can tell if they are anagram");
+    String firstWord = scanner.nextLine();
+    String secondWord = scanner.nextLine();
 
     if (firstWord.length() != secondWord.length()) {
       System.out.println(false);
     } else {
       for (int i = 0; i < firstWord.length(); i++) {
-        // !secondWord.contains('r') = secondWord.indexOf('r') != -1
         if (secondWord.indexOf(firstWord.charAt(i)) == -1) {
           System.out.println(false);
           return;
