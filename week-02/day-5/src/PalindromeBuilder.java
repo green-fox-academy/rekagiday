e089e643c056920cfad09fec015a37468e0f490f
Reka Giday <giday.r@gmail.com>
2017-03-24 11:27:51 +0100

    removed unnecessary function

--- week-02/day-5/src/PalindromeBuilder.java
+++ week-02/day-5/src/PalindromeBuilder.java
@@ -6,17 +6,13 @@ import java.util.Scanner;
 public class PalindromeBuilder {
 
   public static void main(String[] args) {
-    palindrome("add");
-  }
 
-  private static void palindrome(String text) {
     System.out.println("please enter a text you want to turn into a palindrome!");
     Scanner scanner = new Scanner(System.in);
     String original = scanner.nextLine();
     String palindromeSecondpart =new StringBuilder(original).reverse().toString();
     original = original.concat(palindromeSecondpart);
     System.out.println("here's your palindrome: " + original);
-    return;
 
   }
 
