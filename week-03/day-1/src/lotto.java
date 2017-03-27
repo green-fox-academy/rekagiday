e6c2e51623434d070915aed6b36cd8254bcd1138
Reka Giday <giday.r@gmail.com>
2017-03-27 14:22:25 +0200

    hashmap sorted by value

--- week-03/day-1/src/lotto.java
+++ week-03/day-1/src/lotto.java
@@ -2,10 +2,14 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Set;
 
 /**
  * Created by User on 2017. 03. 26..
@@ -31,10 +35,23 @@ public class lotto {
         }
       }
 
-      for (Entry myEntry: map.entrySet()) {
-        System.out.println(myEntry.getKey() + " " + myEntry.getValue());
+      Set<Entry<String, Integer>> set = map.entrySet();
+      List<Entry<String, Integer>> list = new ArrayList<Entry<String, Integer>>(
+          set);
+      Collections.sort(list, new Comparator<Entry<String, Integer>>() {
+
+        public int compare(Map.Entry<String, Integer> o1,
+            Map.Entry<String, Integer> o2) {
+          return o2.getValue().compareTo(o1.getValue());
+        }
+      });
+
+      for (Entry<String, Integer> entry : list) {
+
+        System.out.println(entry.getKey() + " : " + entry.getValue());
       }
-      
+
+
     } catch (IOException e) {
       e.printStackTrace();
     }
