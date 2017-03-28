ff5fc3a20e143a28c3292c1d0d7abba233450dab
Reka Giday <giday.r@gmail.com>
2017-03-28 11:28:15 +0200

    initial commit of positionsquare exercise

---
+++ week-03/day-2/src/PositionSquare.java
@@ -0,0 +1,41 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class PositionSquare {
+
+  public static void mainDraw(Graphics graphics){
+
+    DrawSquare(200, 200, graphics);
+    DrawSquare(20, 20, graphics);
+    DrawSquare(100, 100, graphics);
+
+  }
+
+  private static void DrawSquare(int x, int y, Graphics graphics) {
+
+    graphics.fillRect(x, y, 50, 50);
+
+  }
+
+  //    Don't touch the code below
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setSize(new Dimension(300, 300));
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    jFrame.add(new ImagePanel());
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+  }
+  static class ImagePanel extends JPanel{
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
+
+    }
+  }
+
+}
