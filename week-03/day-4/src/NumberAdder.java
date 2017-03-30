465f2fa8eb66ed63ebd19a9fe841128ae9ce22e0
Reka Giday <giday.r@gmail.com>
2017-03-30 10:05:33 +0200

    initial commit of resursive number adder

---
+++ week-03/day-4/src/NumberAdder.java
@@ -0,0 +1,16 @@
+/**
+ * Created by User on 2017. 03. 30..
+ */
+public class NumberAdder {
+
+  public static void main(String[] args) {
+    add();
+  }
+
+  private static int add(int n) {
+    if ()
+
+    return 0;
+  }
+
+}
