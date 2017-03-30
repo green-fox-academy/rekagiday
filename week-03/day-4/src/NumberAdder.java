9490b188d9550ee54a3d064835e872818c1373b2
Reka Giday <giday.r@gmail.com>
2017-03-30 10:40:30 +0200

    initial commit of recursive numberadder

--- week-03/day-4/src/NumberAdder.java
+++ week-03/day-4/src/NumberAdder.java
@@ -4,13 +4,19 @@
 public class NumberAdder {
 
   public static void main(String[] args) {
-    add();
+    System.out.println(add(10));
   }
 
   private static int add(int n) {
-    if ()
+    if (n == 0) {
+      return 0;
+    }
+    else {
+      int result = n + add(n - 1);
+      System.out.println(n);
+      return result;
+    }
 
-    return 0;
+
   }
-
 }
