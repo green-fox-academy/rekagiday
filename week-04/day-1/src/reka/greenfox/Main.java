1a0dcfb0c2cfe8444a6cfbe18177a66b46b4a33f
Reka Giday <giday.r@gmail.com>
2017-04-03 10:52:36 +0200

    main class updated with blogpost exercise

--- week-04/day-1/src/reka/greenfox/Main.java
+++ week-04/day-1/src/reka/greenfox/Main.java
@@ -1,16 +1,25 @@
 package reka.greenfox;
-import reka.greenfox.cat.Cat;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Cat cirmi = new Cat(8, "brown", "male", 25.0, "siamese");
-        System.out.println(cirmi.getAge());
+  public static void main(String[] args) {
 
-        PostIt first = new PostIt("orange", "Idea 1", "blue" );
-        PostIt second = new PostIt("pink", "Awesome", "black" );
-        PostIt third = new PostIt("yellow", "Superb", "green" );
+    PostIt first = new PostIt("orange", "Idea 1", "blue");
+    PostIt second = new PostIt("pink", "Awesome", "black");
+    PostIt third = new PostIt("yellow", "Superb", "green");
 
-    }
+    BlogPost firstPost = new BlogPost("John Doe", "Lorem Ipsum", "Lorem ipsum dolor sit amet.",
+        "2000.05.04.");
+    BlogPost secondPost = new BlogPost("Tim Urban", "Wait but why",
+        "A popular long-form, stick-figure-illustrated blog about almost everything.",
+        "2010.10.10.");
+    BlogPost thirdPost = new BlogPost("William Turton",
+        "One Engineer Is Trying to Get IBM to Reckon With Trump",
+        "Daniel Hanley, a cybersecurity engineer at IBM, doesn’t want to be the center of attention. "
+            + "When I asked to take his picture outside one of IBM’s New York City offices, he told me that "
+            + "he wasn’t really into the whole organizer profile thing.",
+        "2017.03.28.");
+
+  }
 }
 
