35ad38c6652cf09fe0904957c8107ec53bce32d7
Reka Giday <giday.r@gmail.com>
2017-04-03 11:20:02 +0200

    PostIt class updated with toString method

--- week-04/day-1/src/reka/greenfox/PostIt.java
+++ week-04/day-1/src/reka/greenfox/PostIt.java
@@ -9,10 +9,22 @@ public class PostIt {
   String text;
   String textColor;
 
+  @Override
+  public String toString() {
+    return "PostIt{" +
+        "backgroundColor='" + backgroundColor + '\'' +
+        ", text='" + text + '\'' +
+        ", textColor='" + textColor + '\'' +
+        '}';
+  }
+
+
   public PostIt(String backgroundColor, String text, String textColor) {
     this.backgroundColor = backgroundColor;
     this.text = text;
     this.textColor = textColor;
   }
+
+
 }
 
