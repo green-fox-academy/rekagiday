de28a0e72a1324db2f52c9d9459200e93dcb1735
Reka Giday <giday.r@gmail.com>
2017-04-03 10:52:02 +0200

    first commit of blogpost class

---
+++ week-04/day-1/src/reka/greenfox/BlogPost.java
@@ -0,0 +1,28 @@
+package reka.greenfox;
+
+/**
+ * Created by User on 2017. 04. 03..
+ */
+public class BlogPost {
+//  Create a BlogPost class that has
+//      an authorName
+//      a title
+//      a text
+//      a publicationDate
+
+  String authorName;
+  String title;
+  String text;
+  String publicationDate;
+
+  public BlogPost(String authorName, String title, String text, String publicationDate) {
+    this.authorName = authorName;
+    this.title = title;
+    this.text = text;
+    this. publicationDate = publicationDate;
+
+  }
+
+
+
+}
