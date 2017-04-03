35d9d09559dc5e51ecb6457ef6d051502752670c
Reka Giday <giday.r@gmail.com>
2017-04-03 10:34:04 +0200

    first commit of postit exercise

---
+++ week-04/day-1/src/reka/greenfox/PostIt.java
@@ -0,0 +1,18 @@
+package reka.greenfox;
+
+/**
+ * Created by User on 2017. 04. 02..
+ */
+public class PostIt {
+
+  String backgroundColor;
+  String text;
+  String textColor;
+
+  public PostIt(String backgroundColor, String text, String textColor) {
+    this.backgroundColor = backgroundColor;
+    this.text = text;
+    this.textColor = textColor;
+  }
+}
+
