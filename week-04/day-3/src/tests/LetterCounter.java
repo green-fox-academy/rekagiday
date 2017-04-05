a356d2b1d5bdb28a2844a1593be24096706ec5cf
Reka Giday <giday.r@gmail.com>
2017-04-05 14:35:37 +0200

    lettercounter class and method creater

---
+++ week-04/day-3/src/tests/LetterCounter.java
@@ -0,0 +1,27 @@
+package tests;
+import java.util.HashMap;
+import java.util.Map;
+
+public class LetterCounter {
+
+  public HashMap<Character, Integer> letterCounter(String string) {
+
+    HashMap<Character, Integer> letterList;
+    letterList = new HashMap<>();
+
+    for (int i = 0; i < string.length(); i++) {
+      int counter = 0;
+      char currentLetter = string.charAt(i);
+      if (!letterList.containsKey(string.charAt(i))) {
+        letterList.put(string.charAt(i), 1);
+      }
+      for (int j = 0; j < string.length(); j++) {
+        if (string.charAt(j) == currentLetter) {
+          counter++;
+        }
+      }
+    }
+    return letterList;
+  }
+}
+
