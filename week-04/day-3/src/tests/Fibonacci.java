182cd0384bf970a087883d6b816c4a8c6ad0a129
Reka Giday <giday.r@gmail.com>
2017-04-05 15:22:29 +0200

    initial commit of fibonacci class

---
+++ week-04/day-3/src/tests/Fibonacci.java
@@ -0,0 +1,21 @@
+package tests;
+
+/**
+ * Created by User on 2017. 04. 05..
+ */
+public class Fibonacci {
+
+  public static void main(String[] args) {
+    System.out.println(fibonacci(8));
+  }
+
+  private static int fibonacci(int n) {
+    if (n == 0) {
+      return 0;
+    } else if (n == 1) {
+      return 1;
+    } else {
+      return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+  }
+}
