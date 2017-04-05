3946d33885cb60462309185eed64ecb1599302c3
Reka Giday <giday.r@gmail.com>
2017-04-05 15:18:53 +0200

    lettercounter method fixed, tested, test updated and working

--- week-04/day-3/src/tests/LetterCounter.java
+++ week-04/day-3/src/tests/LetterCounter.java
@@ -11,13 +11,13 @@ public class LetterCounter {
     for (int i = 0; i < string.length(); i++) {
       int counter = 0;
       char currentLetter = string.charAt(i);
-      if (!letterList.containsKey(string.charAt(i))) {
-        letterList.put(string.charAt(i), 1);
-      }
       for (int j = 0; j < string.length(); j++) {
         if (string.charAt(j) == currentLetter) {
           counter++;
         }
+      }
+      if (!letterList.containsKey(string.charAt(i))) {
+        letterList.put(string.charAt(i), counter++);
       }
     }
     return letterList;
--- week-04/day-3/src/tests/LetterCounterTest.java
+++ week-04/day-3/src/tests/LetterCounterTest.java
@@ -1,8 +1,30 @@
 package tests;
 
-/**
- * Created by User on 2017. 04. 05..
- */
-public class LetterCounterTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.HashMap;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class LetterCounterTest {
+  LetterCounter myCounter;
+  HashMap<Character, Integer> testMap;
+
+  @BeforeEach
+  void createString() {
+    myCounter = new LetterCounter();
+    testMap = new HashMap<>();
+    testMap.put('k', 1);
+    testMap.put('i', 2);
+    testMap.put('s', 1);
+    testMap.put('c', 2);
+    testMap.put('a', 1);
+  }
+
+  @Test
+  void letterCounterTest(){
+    System.out.println(myCounter.letterCounter("kiscica"));
+    assertEquals(testMap, myCounter.letterCounter("kiscica"));
+  }
 }
+
