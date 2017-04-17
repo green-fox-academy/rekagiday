11a005e06ba6b19d5d943991bd3852c13eeb0343
Reka Giday <giday.r@gmail.com>
2017-04-17 20:53:31 +0200

    create classes for zoo exercise

---
+++ week-06/day-1/src/Bird.java
@@ -0,0 +1,23 @@
+/**
+ * Created by User on 2017. 04. 17..
+ */
+public class Bird extends Oviparous {
+
+  public Bird(String name) {
+    super(name);
+  }
+
+  @Override
+  void eat() {
+  }
+
+  @Override
+  void sleep() {
+  }
+
+  @Override
+  void greet() {
+  }
+
+}
+
---
+++ week-06/day-1/src/Reptile.java
@@ -0,0 +1,22 @@
+/**
+ * Created by User on 2017. 04. 17..
+ */
+public class Reptile extends Oviparous {
+
+  public Reptile(String name) {
+    super(name);
+  }
+
+  @Override
+  void eat() {
+  }
+
+  @Override
+  void sleep() {
+  }
+
+  @Override
+  void greet() {
+  }
+
+}
