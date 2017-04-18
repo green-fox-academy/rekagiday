b654628dc00a4583745cf2871e7cabe83e2ad973
Reka Giday <giday.r@gmail.com>
2017-04-18 12:39:55 +0200

    paths and exceptions added to copy exercise

--- exam1/src/Copy.java
+++ exam1/src/Copy.java
@@ -1,23 +1,34 @@
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by User on 2017. 04. 18..
  */
 public class Copy {
 
+
   public static void main(String[] args) {
-    // This should be the basic replica of the 'cp' command
-    // If ran from the command line without arguments
-    // It should print out the usage:
-    // copy [source] [destination]
-    // When just one argument is provided print out
-    // No destination provided
-    // When both arguments provided and the source is a file
-    // Read all contents from it and write it to the destination
 
     if (args.length == 0) {
       System.out.println("copy [source] [destination]");
-    } else if (args.length == 1) {
+    } else if (args.length <= 2) {
       System.out.println("No destination provided");
-//    } else if (args.length == 1 && args[1].contains(path)) {
+    } else if (args.length == 3 && args[0].equals("copy")) {
+      try {
+        Path readFrom = Paths.get(args[1]);
+        List<String> content = new ArrayList<>();
+        List<String> lines = Files.readAllLines(readFrom);
+        for (int i = 0; i < lines.size(); i++) {
+          content.add(lines.get(i));
+        }
+        Path writeTo = Paths.get(args[2]);
+        Files.write(writeTo, content);
+      } catch (Exception e) {
+        System.out.println("Source or destination is not a file");
+      }
     }
   }
 }
