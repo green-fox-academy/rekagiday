b2d7303ad67d0de66344bd7c872be2197089de84
Reka Giday <giday.r@gmail.com>
2017-04-19 11:01:28 +0200

    initial commit of parkinglot exercise

---
+++ week-06/day-2/src/ParkingLot/Car.java
@@ -0,0 +1,21 @@
+package ParkingLot;
+
+/**
+ * Created by User on 2017. 04. 19..
+ */
+public class Car {
+
+  public enum carType {OPEL, BMW, MERCEDES, AUDI, SUZUKI}
+
+  public enum carColor {WHITE, BLACK, RED, SILVER, YELLOW}
+
+  public Car(carType type, carColor color) {
+  }
+
+  public static void main(String[] args) {
+
+    
+
+
+  }
+}
