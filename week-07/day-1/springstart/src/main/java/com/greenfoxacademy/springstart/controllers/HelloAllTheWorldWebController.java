f1bfe0996bebfec00c4f2c6a771b8f8fe806d415
Reka Giday <giday.r@gmail.com>
2017-05-02 17:18:55 +0200

    add randomcolor and randomsize methods

--- week-07/day-1/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloAllTheWorldWebController.java
+++ week-07/day-1/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloAllTheWorldWebController.java
@@ -23,9 +23,36 @@ public class HelloAllTheWorldWebController {
 
 
   @RequestMapping(value = "/web/helloall")
-  public String greeting(@RequestParam(value = "name", defaultValue = "World") String name, Model model) {
+  public String greeting(@RequestParam(value = "name", defaultValue = "World") String name,
+      Model model) {
     model.addAttribute("name", name);
     model.addAttribute("hellos", hellos);
+    model.addAttribute("color", randomColors());
+    model.addAttribute("fontSize", randomSize());
     return "helloall";
+  }
+
+  public String[] randomColors() {
+    String[] color = new String[hellos.length];
+
+    for (int i = 0; i < hellos.length; i++) {
+      int r = (int) (Math.random() * 256);
+      int g = (int) (Math.random() * 256);
+      int b = (int) (Math.random() * 256);
+      String rgb = "rgb(" + r + "," + g + "," + b + ")";
+      color[i] = rgb;
+    }
+    return color;
+  }
+
+  public String[] randomSize() {
+    String[] fontSize = new String[hellos.length];
+
+    for (int i = 0; i < hellos.length; i++) {
+      int randomSize = (int) (Math.random() * 45 + 15);
+      String size = randomSize + "px";
+      fontSize[i] = size;
+    }
+    return fontSize;
   }
 }
