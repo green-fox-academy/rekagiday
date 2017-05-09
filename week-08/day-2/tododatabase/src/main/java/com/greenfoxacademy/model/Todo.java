7b10092af5a3ae499c9e3fe3fbc86fbec082a3cf
Reka Giday <giday.r@gmail.com>
2017-05-09 14:08:17 +0200

    initial commit

---
+++ week-08/day-2/tododatabase/src/main/java/com/greenfoxacademy/model/Todo.java
@@ -0,0 +1,28 @@
+package com.greenfoxacademy.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Todo {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private long id;
+  private String title;
+  private boolean isUrgent;
+  private boolean isDone;
+
+  public Todo() {
+  }
+
+  public Todo(String title) {
+    this.title = title;
+  }
+}
