4ea2e8ccb17a0aa0cbcaf1d641708892d28e33bd
Reka Giday <giday.r@gmail.com>
2017-07-03 11:27:17 +0200

    add second exercise to cs project

--- cs_workshops/Program.cs
+++ cs_workshops/Program.cs
@@ -13,13 +13,20 @@ namespace ConsoleApp1
 
 			int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
 
-			IEnumerable<int> secondList = n.Where(y => y % 2 == 0);
+			IEnumerable<int> firstList = n.Where(y => y % 2 == 0);
+			double averageOfOddNumbers = n.Where(y => y % 2 == 1).Average();
 
-			foreach (int e in secondList)
+			foreach (int e in firstList)
 			{
 				Console.WriteLine(e);
 			}
+			
+			Console.WriteLine("average of odd numbers: " + averageOfOddNumbers);
+
 			Console.ReadLine();
+
+
+
 		}
 	}
 }
