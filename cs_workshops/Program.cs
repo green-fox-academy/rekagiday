6f32e123dcab38ae07921fe86a2c8a5136e24315
Reka Giday <giday.r@gmail.com>
2017-07-03 11:16:43 +0200

    initial commit of cs exercises

---
+++ cs_workshops/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+
+			IEnumerable<int> secondList = n.Where(y => y % 2 == 0);
+		}
+	}
+}
