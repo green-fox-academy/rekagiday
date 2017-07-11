7a29e5b10ef21716c6ac71a980122445d79aca32
Reka Giday <giday.r@gmail.com>
2017-07-11 15:04:55 +0200

    anagram checker exercise and unit test

---
+++ cs_workshops/TestExercises/TestExercises/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestExercises
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine(AnagramCheck("alma", "lawma"));
+			Console.ReadLine();
+		}
+
+		public static bool AnagramCheck(String a, String b)
+		{
+			string aa = String.Concat(a.OrderBy(c => c));
+			string bb = String.Concat(b.OrderBy(c => c));
+
+			if (aa == bb)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
+}
---
+++ cs_workshops/TestExercises/TestExercisesTests/ProgramTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestExercises;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestExercises.Tests
+{
+	[TestClass()]
+	public class ProgramTests
+	{
+		[TestMethod()]
+		public void AnagramCheckTest()
+		{
+			Assert.AreEqual(true, Program.AnagramCheck("alma", "lama"));
+		}
+	}
+}
