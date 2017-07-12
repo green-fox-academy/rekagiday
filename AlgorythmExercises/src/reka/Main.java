357505be8a7d5bfc27fc533ee1e0413e809c7224
Reka Giday <giday.r@gmail.com>
2017-07-12 15:26:11 +0200

    add euler exercise 1

--- AlgorythmExercises/src/reka/Main.java
+++ AlgorythmExercises/src/reka/Main.java
@@ -19,8 +19,20 @@ public class Main {
         negativeCount++;
       }
     }
+
     System.out.println(positiveCount / n);
     System.out.println(negativeCount / n);
     System.out.println(zeroCount / n);
+    System.out.println(multiplesOf3and5(1000));
+  }
+
+  public static int multiplesOf3and5(int n) {
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+      if (i % 3 == 0 || i % 5 == 0) {
+        sum += i;
+      }
+    }
+    return sum;
   }
 }
